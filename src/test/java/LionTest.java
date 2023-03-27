4252ffeeb7d9b86130ab987803246389049c8135
Azima Assan <assanazima@yandex.kz>
2023-03-27 19:58:23 +0600

    add Lion Test

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,51 +1,34 @@
-import com.example.Lion;
-import org.junit.runners.Parameterized;
 import com.example.Feline;
+import com.example.Lion;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 import static junit.framework.TestCase.assertEquals;
 
-
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
-    private final Feline feline;
-    private final Lion lion;
-    private final boolean hasMane;
+    private final String sex = "Самка";
+    @Mock
+    private Feline felineMock;
 
-    @Parameterized.Parameters
-    public static Object[][] getData() {
-
-        return new Object[][] {
-                {Mockito.mock(Feline.class), "Самец", true},
-                {Mockito.mock(Feline.class), "Самка", false},
-        };
-    }
-
-    public LionTest(Feline feline, String sex, boolean hasMane) throws Exception {
-        this.lion = new Lion(feline, sex);
-        this.feline = feline;
-        this.hasMane = hasMane;
-    }
-
-    @Test
-    public void hasManeTest() {
-        assertEquals("Не совпадает hasMane", hasMane, lion.doesHaveMane());
-    }
 
     @Test
     public void getFoodTest() throws Exception {
+        Lion lion = new Lion(felineMock, sex);
         List<String> meats = List.of("A", "B", "C");
-        Mockito.when(feline.getFood("Хищник")).thenReturn(meats);
+        Mockito.when(felineMock.getFood("Хищник")).thenReturn(meats);
         assertEquals("Список еды не совпадает", meats, lion.getFood());
     }
 
     @Test
-    public void getKittensTest() {
+    public void getKittensTest() throws Exception {
+        Lion lion = new Lion(felineMock, sex);
         int kittensCount = 456;
-        Mockito.when(feline.getKittens()).thenReturn(kittensCount);
+        Mockito.when(felineMock.getKittens()).thenReturn(kittensCount);
         assertEquals("Количество котят не совпадает", kittensCount, lion.getKittens());
     }
 }
